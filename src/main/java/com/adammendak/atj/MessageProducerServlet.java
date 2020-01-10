751b1bfbf2740c77c8860c4627b5a2e6e8d951e1
adammendak <adam.mendak@gmail.com>
2020-01-10 16:11:34 +0100

    more

--- src/main/java/com/adammendak/atj/MessageProducerServlet.java
+++ src/main/java/com/adammendak/atj/MessageProducerServlet.java
@@ -1,5 +1,11 @@
 package com.adammendak.atj;
 
+import com.sun.messaging.ConnectionFactory;
+import com.sun.messaging.Topic;
+import com.sun.messaging.jms.JMSException;
+
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,13 +16,50 @@ import java.io.IOException;
 @WebServlet(
         asyncSupported = true,
         description = "jms message producer",
-        urlPatterns = { "/produce" })
+        urlPatterns = {"/produce"})
 public class MessageProducerServlet extends HttpServlet {
+
+    private static final String RESPONSE_TEMPLATE = "Wiadomość : %s, wysłana";
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        //todo zrobic to gowno jutro z rana jak smietana
+        String message = String.valueOf(request.getParameter("value"));
 
+        try {
+            JMSPublisher.publishMessage(message);
+        } catch (javax.jms.JMSException e) {
+            e.printStackTrace();
+            respond(request, response, e.getMessage());
+        }
+
+        respond(request, response, String.format(RESPONSE_TEMPLATE, message));
+
+    }
+
+    private void respond(HttpServletRequest request, HttpServletResponse response, String responseValue)
+            throws ServletException, IOException {
+        request.setAttribute("result", responseValue);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+
+    private static class JMSPublisher {
+
+        private static final String TOPIC_NAME = "ATJTopic";
+
+        public static void publishMessage(String message) throws javax.jms.JMSException {
+            ConnectionFactory connectionFactory = new
+                    com.sun.messaging.ConnectionFactory();
+            JMSContext jmsContext = connectionFactory.createContext();
+            JMSProducer jmsProducer = jmsContext.createProducer();
+
+            try {
+                Topic topic = new com.sun.messaging.Topic(TOPIC_NAME);
+                    jmsProducer.send(topic, message);
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+            jmsContext.close();
+        }
     }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -6,11 +6,33 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.util.Objects" %>
 <html>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    dupa test
+<% Object result = request.getAttribute("result"); %>
+    <div class="container">
+        <p align="center">
+            <b>Producent wiadomości dla tematu JNDI Name - jms/ATJTopic</b>
+        </p>
+        <form method="GET" action="produce">
+            <div class="form-group">
+                <label for="messageValue">Teskst wiadomości:</label>
+                <input type="text" class="form-control" id="messageValue" name="value" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Ślij!</button>
+        </form>
+    </div>
+    <table class="table">
+        <tr>
+            <td>
+                <p align="center">
+                    <b><%= Objects.isNull(result) ? "" : String.valueOf(result) %></b>
+                </p>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
